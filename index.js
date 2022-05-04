6ae06ee2bdc4eed1f35c43fe05ba7ba95705595d
sazidahmednassir <sazidahmednassir@gmail.com>
2022-05-04 09:07:25 -0800

    firstsix query added

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 const express = require('express');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 var cors = require('cors')
+
 
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT  ||  5000;
+
+
+const port = process.env.PORT || 5000;
 
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2yyiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('hi')
-  // perform actions on the collection object
-  // client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   console.log('hi')
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run(){
+  try{
+    await client.connect();
+    const mobileCollection = client.db("smartphoneinventory").collection("mobile");
+
+    //firstsixmobile data  
+    app.get("/firstmobile", async (req, res) => {
+      const q = req.query;
+     
+
+      const cursor = mobileCollection.find( q);
+
+      const result = await cursor.toArray();
+
+      res.send(result);
+
+
+    });
+
+    console.log("db")
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
